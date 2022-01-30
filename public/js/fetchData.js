bcf7368ed11ebcf8fe91ef11d5bffb3d957463df
Skydrow91 <64668442+Skydrow91@users.noreply.github.com>
2022-01-30 15:47:28 +0100

    Documenter le code

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -2,9 +2,18 @@ window.addEventListener('DOMContentLoaded',()=>{
   const pokeP=document.getElementById('pokeInfo')
   const pokeDiv=document.getElementById('pokemon-info')
   const pokeAbilityBtn=document.getElementById('ability')
-  
-  // Fonction qui va chercher les API pokemons sur le site pokémon
 
+/**
+ *
+ * @async
+ * @function fetchPokemon - Cherche un pokémon
+ * @param {number} pokedexNum - Cherche un pokémon aléatoire dans le pokédex pour le choisir
+ * @param {string} foundPokemon -Nom du pokémon trouvé
+ * @param {string} jsonPokemon - 
+ * @param {object} pokeInfo - Retourne les infos du pokémon
+ * @return {Promise} -Retourne le nom du pokémon.
+ */
+  
   const fetchPokemon=async ()=>{
     const pokedexNum=Math.floor(Math.random() * 897)
     let foundPokemon=''
@@ -31,8 +40,20 @@ window.addEventListener('DOMContentLoaded',()=>{
     pokeAbilityBtn.removeAttribute('disabled')
   }
 
-  //Fonction qui va chercher les API techniques des pokémons sur le site.
 
+  
+/**
+
+ *
+ * @async
+ * @function fetchPokemonAbilities - Cherche une technique pokémon
+ * @param {number} pokedexNum - Cherche un pokémon aléatoire dans le pokédex pour le choisir
+ * @param {string} foundAbilities -Cherche une technique pokémon
+ * @param {string} jsonAbilities - 
+ * @param {string} abilityToDisplay -Nom de la technique à afficher
+ * @return {Promise} -Retourne la technique que le pokémon à appris.
+ */
+ 
   const fetchPokemonAbilities=async()=>{
     const pokedexNum=Math.floor(Math.random() * 266)
     let foundAbilities=''
@@ -62,23 +83,42 @@ window.addEventListener('DOMContentLoaded',()=>{
 
     pokeAbility.innerText=`It now knows the move ${abilityToDisplay}!`
   }
-  //Fonction pour invoquer le pokémon
 
+
+  /**
+   * @function  invoquePokemon -Fonction pour invocation d'un pokémon
+   * @function pokeBtn - Bouton pour invoquer un pokémon
+   * @param  {string} fetchPokemon - 
+   * @param  {string} pokeDiv.appendChild -
+   */
+  
   const invoquePokemon=()=>{
     const pokeBtn=document.getElementById('pokemon')
     pokeBtn.addEventListener('click', fetchPokemon)
     pokeDiv.appendChild(pokeP)
   }
 
-  //Fonciton pour utiliser la technique du pokémon
+
+  
+/**
+ * @function  pokeAbility  -Fonction pour chercher une technique de pokémon
+ * @function pokeAbilityBtn - Bouton à appuyer pour chercher une technique de pokémon
+ * @param  {string} fetchPokemonAbilities - Recherche d'un technique pokémon
+ * @param  {string} pokeDiv.appendChild
+ */
 
   const pokemonAbility=()=>{
     pokeAbilityBtn.addEventListener('click', fetchPokemonAbilities)
     pokeDiv.appendChild(pokeAbility)
   }
   
-  //Fonction pour commencer le combat pokémon (invocation du pokémon et des techniques).
   
+  /**
+   * @function functionstartAll - Fonction pour commencer la création du pokémon (invocation du pokémon et de la technique attribuée).
+   * @param  {string} invoquePokemon
+   * @param  {string} pokemonAbility
+   */
+
   (function startAll(){
     invoquePokemon()
     pokemonAbility()
